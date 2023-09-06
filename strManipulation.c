0ce95d44185b1cc0fc51be29a41207f50927b089
Erdey Syoum <erdeysyoum@gmail.com>
2023-09-06 10:57:54 +0300

    string Manipulation

--- strManipulation.c
+++ strManipulation.c
@@ -2,18 +2,23 @@
 
 int main() 
 {
-    /* cstr (copystr is needed because if we use the str instead it will go the end and it will not be used to get the original strings) */
+    /* cstr (copystr is needed because if we use the str instead it will go
+       to the end and it will not be used to get the original strings) */
     char *str, *cstr; 
     str = "      ALX School         #cisfun      ";
-    cstr = str;
-    int i, j, len = 0;
+    cstr =str;
+    int i, j, len = 0, nspace = 0;
     for(i = 0; *cstr != '\0'; cstr++)
     {
         len++;
+	if (*cstr == ' ')
+		nspace++;
     }
     for (i = 0; i < len; i++)
-    printf("%c", str[i]);
+	    printf("%c", str[i]);
     printf(" has %d characters\n ", len);
-    
+    for (i = 0; i < len; i++)
+	    printf("%c", str[i]);
+    printf(" has %d spaces \n", nspace);
     return 0;
 }
