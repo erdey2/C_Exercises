caa60ca3810c10a9eaf89d5ca6c779337adcbec2
Erdey Syoum <erdeysyoum@gmail.com>
2023-09-15 15:27:55 +0300

    Format Specifier Printf

--- format_printf.c
+++ format_printf.c
@@ -6,11 +6,21 @@ int main()
     short b = 97;
     long c = 97;
     long long d = 97;
+    int unsigned short e = 97;
+
+    int f = 047;
+    long g = 0x9A;
     
     printf("int: %d size: %ld bytes\n", a, sizeof(int));
     printf("short: %hd size: %ld bytes\n", b, sizeof(short));
     printf("long: %ld size: %ld bytes\n", c, sizeof(long));
     printf("long long: %lld size: %ld bytes\n", d, sizeof(long long));
+
+    printf("unsiged short: %hu size: %ld bytes\n", e, sizeof(short));
+
+    printf("Octal int: %o size: %ld bytes\n", f, sizeof(f));
+    printf("Hexadecimal long: %lx size: %ld bytes\n", g, sizeof(g));
+    
 
     return 0;
 }
