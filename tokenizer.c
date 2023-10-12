d294697e414722ef16d1b9768b3d26c424668913
henok-yes <henokakalu1994@gmail.com>
2023-10-12 05:38:38 -0700

    tokenizer.c file added and main.c shell.h and  string_copier.c modified

---
+++ tokenizer.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+int tokenzer(char *copy_str,  char *loop_argv[])
+{
+	char *token = NULL, *delim = " ";
+	size_t count = 0;
+	token = strtok(copy_str, delim);
+	while(token)
+
+	{
+		loop_argv[count] = token;
+		token = strtok(NULL,delim);
+		count++;
+	}
+	loop_argv[count] = NULL;
+	return (count);
+}
