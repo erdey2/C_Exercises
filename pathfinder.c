3b28eceebf99ae201acb50ef9bde57e1b65f8ada
erdey2 <erdeysyoum@gmail.com>
2023-10-13 08:22:08 -0700

    pathfinder & shell changed

--- pathfinder.c
+++ pathfinder.c
@@ -1,46 +1,43 @@
 #include "shell.h" 
+
 int pathfinder(char *argv[])
 {
   
-    char *delim  = ":", *my_path,*copy_path, *hyphen = "/", *null_char = "\0", *cmd;
+    char *delim  = ":", *my_path, *copy_path, *hyphen = "/", *null_char = "\0", *cmd;
     char *path_argv[1024];
     struct stat buffer;
-    int i, count;
+    int i;
+    size_t count;
     char *mypath;
     my_path = getenv("PATH");
     
     if (my_path)
     {
-      
-      for(count = 0;my_path[count],count ++);
-      copy_path = malloc(sizeof(char)*(count +3));
-        string_copier(mypath,copy_path);
-        tokenzer(copy_path, path_argv,delim);
-        i=0;
-        while (path_argv[i]!= NULL)
-        {
-            _strcat(path_argv[i],"/");
-            _strcat(path_argv[i],argv[0]);
-            _strcat(path_argv[i],"\0");
-
-          if (stat(path_argv[i], &buffer) == 0)
-
-            {
-            argv[0] = path_argv[i];
-            free(copy_path);
-            return (0);
-            }
-    else
-    {
-
-        perror("error");
-        return (4);
-    }
-            
-            i++;
- 
-           }
-
+	   for(count = 0; my_path[count]; count++)
+	   ;
+	   copy_path = malloc(sizeof(char) * (count + 1));
+	   string_copier(mypath, copy_path);
+	   tokenzer(copy_path, path_argv, delim);
+	   i = 0;
+	   while (path_argv[i]!= NULL)
+	    {
+		    _strcat(path_argv[i], "/");
+		    _strcat(path_argv[i], argv[0]);
+		    _strcat(path_argv[i], "\0");
+		    
+		    if (stat(path_argv[i], &buffer) == 0)
+		    {
+			    argv[0] = path_argv[i];
+			    free(copy_path);
+			    return (0);
+		    }
+		    else
+		    {
+			    perror("error");
+			    return (4);
+		    }
+		    i++;
+	    }
     }
 }
 
--- shell.h
+++ shell.h
@@ -10,7 +10,8 @@
 #include <sys/wait.h>
 void prompt();
 int command_excute(char *loop_argv[]);
-int string_copier(c
-har *str, char *copy);
+int string_copier(char *str, char *copy);
 int compare_str(char *src, char *str);
+int _strcat(char *s1, char *s2);
+int tokenzer(char *copy_str,  char *loop_argv[], char *delim);
 #endif
