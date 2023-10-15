59d6b33832edd2d1b6e9de663c8584c4a2cc304a
erdey2 <erdeysyoum@gmail.com>
2023-10-15 06:56:35 -0700

    main updated

--- main.c
+++ main.c
@@ -1,32 +1,25 @@
 #include "shell.h"
 
 /**
- * compare_string - compare two strings.
- * @src: pointer to the first string to be compared.
- * @str: pointer to the second string to be compared.
- *
- * Return: 0 if the strings are equal otherwise -1
+ * main - the entry point.
+ * Return: 0
  */
-
-int main()
+int main(void)
 {
-	size_t n,compared_result;
-	char *str, *copy_str, *my_exit = "exit", *delim = " ",*cmd,*argv1,*my_env = "env";
-	char *loop_argv[1024];
+	size_t n = 0, compared_result, no_read;
+	char *str = NULL, *copy_str, *my_exit = "exit", *delim = " ", *cmd, *argv1;
+	char *loop_argv[1024], *my_env = "env";
 	pid_t pid;
-	int no_read ;
-	
+
 	while (1)
 	{
 		prompt();
-		str = NULL,n = 0;
-		no_read = getline (&str, &n, stdin);
+		no_read = getline(&str, &n, stdin);
 		if (no_read == -1)
 		{
 			if (feof(stdin))
 			{
-				write(1, "\n", 1);
-				exit(0);
+				write(1, "\n", 1), exit(0);
 			}
 			else
 			{
@@ -34,50 +27,40 @@ int main()
 				return (1);
 			}
 		}
-	 	if((str[0]=='\n') && (no_read==1))
+		if ((str[0] == '\n') && (no_read == 1))
 			continue;
 		copy_str = malloc(sizeof(char) * no_read);
 		string_copier(str, copy_str);
-		compared_result =  compare_str(copy_str, my_exit);
-		if(compared_result == 0)
+		compared_result = compare_str(copy_str, my_exit);
+		if (compared_result == 0)
 			exit(0);
-		compared_result =  compare_str(copy_str, my_env);
-   if(compared_result == 0)
-   {
-	print_env_variable();
-	continue;
-   }
+		compared_result = compare_str(copy_str, my_env);
+		if (compared_result == 0)
+		{
+			print_env_variable();
+			continue;
+		}
+		tokenzer(copy_str, loop_argv, delim);
+		cmd = loop_argv[0], argv1 = pathfinder(cmd);
+		if (argv1 == NULL)
+		{
+			perror("./shell");
+			continue;
+		}
+		loop_argv[0] = argv1, pid = fork();
+		if (pid < 0)
+		{
+			perror("./shell");
+			return (2);
+		}
+		else if (pid == 0)
+			command_excute(loop_argv);
+		else
+		{
+			int status;
 
-
-	tokenzer(copy_str,loop_argv,delim);
-	/*old_argv=loop_argv[0];*/
-	cmd = loop_argv[0];
-argv1= pathfinder(cmd);
-if (argv1 == NULL)
-  {
-perror("./shell");
-continue;
-
-  }
- loop_argv[0]=argv1;
-pid = fork();
-	if (pid < 0)
-	{
-		perror("./shell");
-		return (2);
+			wait(&status), free(str), free(copy_str);
+		}
 	}
-	else if (pid == 0)
-command_excute(loop_argv); 
-	else
-	{
-		int status;
-            wait(&status); 
-		
-		free(str);
-		free(copy_str);
-	
-		
-	}
-}
-return (0);
+	return (0);
 }
