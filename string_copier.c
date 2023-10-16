dd2a4193c5d904ba33f37ab227ca9dd9f5432b65
erdey2 <erdeysyoum@gmail.com>
2023-10-16 01:23:00 -0700

    string_copier.c betty checked

--- string_copier.c
+++ string_copier.c
@@ -3,14 +3,14 @@
 /**
  * string_copier - copy string from its origin
  * @str: pointer to the source string
- * @copy: pointer to the destination string
+ * @cpy_of_str: pointer to the destination string
  * Return: 0
  */
 int string_copier(char *str, char *cpy_of_str)
 {
 	int i = 0;
-	ssize_t compared_result ;
-	char *my_exit = "exit", *my_env ="env";
+	ssize_t compared_result;
+	char *my_exit = "exit", *my_env = "env";
 
 	while (str[i])
 	{
@@ -18,16 +18,15 @@ int string_copier(char *str, char *cpy_of_str)
 		i++;
 	}
 	if (cpy_of_str[i - 1] == '\n')
-		cpy_of_str[i - 1] ='\0';
-	compared_result =  compare_str (cpy_of_str, my_exit);
-		if(compared_result == 0)
-			exit(0);
-		compared_result = compare_str(cpy_of_str, my_env);
-		if (compared_result == 0)
-		{
-			print_env_variable();
-			return (0);
-		}
-
+		cpy_of_str[i - 1] = '\0';
+	compared_result = compare_str(cpy_of_str, my_exit);
+	if (compared_result == 0)
+		exit(0);
+	compared_result = compare_str(cpy_of_str, my_env);
+	if (compared_result == 0)
+	{
+		print_env_variable();
+		return (0);
+	}
 	return (0);
 }
