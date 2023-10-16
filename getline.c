0827423de9104b0805c459ec38a52c74374cc571
erdey2 <erdeysyoum@gmail.com>
2023-10-16 00:29:03 -0700

    getline.c betty checked

--- getline.c
+++ getline.c
@@ -1,10 +1,18 @@
 #include "shell.h"
-int shellgetline(char **str,size_t *n)
-{
-    ssize_t num_of_read;
-   
 
-num_of_read = getline (str, n, stdin);
+/**
+ * shellgetline - reads one line
+ * @str: the address of the first character of the string
+ * @n: the buffer size
+ *
+ * Return: the number of characters read
+ */
+
+int shellgetline(char **str, size_t *n)
+{
+	ssize_t num_of_read;
+
+	num_of_read = getline(str, n, stdin);
 	if (num_of_read == -1)
 	{
 		if (feof(stdin))
@@ -12,16 +20,11 @@ num_of_read = getline (str, n, stdin);
 
 			exit(0);
 		}
-        else
-        {
-
-            perror("./shell");
-            return(1);
- 
-        }
-	
+		else
+		{
+			perror("./shell");
+			return (1);
+		}
 	}
- 
-    return (num_of_read);
-
+	return (num_of_read);
 }
