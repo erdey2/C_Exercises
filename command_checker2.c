509d4840e48f471e4a50b7efddd1b39ece865f66
erdey2 <erdeysyoum@gmail.com>
2023-10-17 08:00:59 -0700

    command_checker2.c betty checked

--- command_checker2.c
+++ command_checker2.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 
 /**
- * set_env - sets an array of Enviroment Variables
- * @evi: Array of pointers 
+ * _setenv - sets an array of Enviroment Variables
+ * @evi: Array of pointers
  */
 
 void _setenv(char **evi)
@@ -15,7 +15,7 @@ void _setenv(char **evi)
 }
 
 /**
- * unset_env - Frees the environment variables
+ * _unset - Frees the environment variables
  * @env:  Array of pointer of Environment variables
  */
 void _unset(char **env)
